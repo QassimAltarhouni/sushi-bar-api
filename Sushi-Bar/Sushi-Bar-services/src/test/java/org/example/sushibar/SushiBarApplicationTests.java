65001f6b125bbe24fd144e257a7c510454e909fc
Mohammedkasemaltarhony <86439517+Mohammedkasemaltarhony@users.noreply.github.com>
2025-07-31 00:59:37 +0200

    Use mongodb instead H2
    Use mongodb instead H2

--- Sushi-Bar/Sushi-Bar-services/src/test/java/org/example/sushibar/SushiBarApplicationTests.java
+++ Sushi-Bar/Sushi-Bar-services/src/test/java/org/example/sushibar/SushiBarApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.sushibar;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SushiBarApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package org.example.sushibar;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SushiBarApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
